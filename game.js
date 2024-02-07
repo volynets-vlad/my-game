d560807d562940e30edf143709583ea7055e38ac
volynets-vlad <158173204+volynets-vlad@users.noreply.github.com>
2024-02-07 19:49:26 +0200

    вніс зміни в гру

--- game.js
+++ game.js
@@ -1,32 +1,30 @@
 class Main extends Phaser.Scene {
     preload() {
-        this.load.spritesheet('plane', 'assets/planesheet.png', { frameWidth: 98, frameHeight: 83 });
+        this.load.image('plane', 'assets/plane.png',);
         this.load.image('pipe', 'assets/pipe.png');
+        this.load.image('sky', 'assets/sky.png');
         this.load.audio('jump', 'assets/jump.wav');
     }
 
     create() {
+        this.add.image(400, 300, 'sky');
         //Додаємо літак на сцену
         this.plane = this.physics.add.sprite(50, 100, 'plane')
         //Масштабуємо літак
-        this.plane.setScale(0.65, 0.65);
+        this.plane.setScale(0.6, 0.6);
         //Встановлюємо опорну точку літака
         this.plane.setOrigin(0, 0.5);
-        this.anims.create({
-            key: "planeAnimation",
-            frames: this.anims.generateFrameNumbers('plane', { frames: [0, 1, 3, 2] }),
-            frameRate: 10,
-            repeat: -1
-        });
         this.plane.play("planeAnimation");
 
-        this.plane.body.gravity.y = 1000;
+        this.plane.body.gravity.y = 500;
         this.spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         this.score = 0;
          this.labelScore = this.add.text(140, 20, "Created by Covid 20 pro msax ultra nfc 5g", { fontSize: 10, color: "white" });
          this.labelScore = this.add.text(15, 15, "0", { fontSize: 30, color: "white" });
-       
+
         this.pipes = this.physics.add.group();
+
+        
 
         this.timedEvent = this.time.addEvent({
             delay: 1500,
@@ -43,7 +41,7 @@ class Main extends Phaser.Scene {
             this.plane.angle += 1;
         }
 
-        if (this.plane.y < 0 || this.plane.y > 490) {
+        if (this.plane.y < 0 || this.plane.y > 600) {
             this.scene.restart();
         }
         if (this.spaceBar.isDown) {
@@ -57,28 +55,28 @@ class Main extends Phaser.Scene {
             duration: 100,
             repeat: 1
         });
-        this.plane.body.velocity.y = -350;
+        this.plane.body.velocity.y = -170;
             var sound = this.sound.add('jump');
         sound.play();
     }
     //Функція для створення блоку труби
     addOnePipe(x, y) {
-        var pipe = this.physics.add.sprite(x, y, 'pipe');
+        var pipe = this.physics.add.sprite(800, y, 'pipe');
         pipe.setOrigin(0, 0);
         this.pipes.add(pipe);
-        pipe.body.velocity.x = -300;
+        pipe.body.velocity.x = -250;
 
         pipe.collideWorldBounds = true;
         pipe.outOfBoundsKill = true;
     }
     //Функція створення труби (стовпчик блоків)
     addRowOfPipes() {
-        var hole = Math.floor(Math.random() * 5) + 1;
+        var hole = Math.floor(Math.random() * 4) + 1;
         this.score += 1;
         this.labelScore.text = this.score;
         for (var i = 0; i < 8; i++) {
             if (!(i >= hole && i <= hole + 2))
-                this.addOnePipe(400, i * 60 + 10);
+                this.addOnePipe(400, i * 65 + 10);
         }
     }
     hitPipe () {
@@ -106,8 +104,8 @@ class Main extends Phaser.Scene {
 
 const config = {
     type: Phaser.AUTO,
-    width: 400,
-    height: 490,
+    width: 800,
+    height: 540,
     scene: Main, // Цю сцену ми створимо на 4-му кроці
     backgroundColor: '#71c5cf',
     physics: {
